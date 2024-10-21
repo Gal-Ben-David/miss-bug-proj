cc4d86089f824b0e36bacfdde4a5d0dfbfbb5ea6
Gal Ben David <gal.benda3@gmail.com>
2024-10-21 17:23:47 +0300

    Provide API for Bugs CRUDL

--- server.js
+++ server.js
@@ -1,6 +1,70 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+
+import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
 
 const app = express()
+// const bugs = JSON.parse(fs.readFileSync('data/bug.json', 'utf8'))
 
-app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+// app.get('/', (req, res) => res.send('Hello there'))
+
+
+// app.get('/api/bug', (req, res) => {
+//     res.send(bugs)
+// })
+
+// app.get('/api/bug/:bugId', (req, res) => {
+//     const { bugId } = req.params
+//     const bug = bugs.find(bug => bug._id === bugId)
+//     if (bug) res.send(bug)
+//     else res.status(404).send('Bug not found')
+// })
+
+
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+        .then(bugs => res.send(bugs))
+        .catch(err => {
+            loggerService.error('Cannot get cars', err)
+            res.status(500).send('Cannot get cars')
+        })
+})
+
+app.get('/api/bug/save', (req, res) => {
+    const bugToSave = {
+        _id: req.query._id,
+        title: req.query.title,
+        severity: req.query.severity,
+        createdAy: Date.now()
+    }
+
+    bugService.save(bugToSave)
+        .then(savedBug => res.send(savedBug))
+        .catch(err => {
+            loggerService.error('Cannot save bug', err)
+            res.status(500).send('Cannot save bug')
+        })
+})
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const { bugId } = req.params
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            loggerService.error('Cannot get bug', err)
+            res.status(500).send('Cannot get bug')
+        })
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => res.send(`Bug ${bugId} removed successfully!`))
+        .catch(err => {
+            loggerService.error('Cannot remove bug', err)
+            res.status(500).send('Cannot remove bug')
+        })
+})
+
+app.listen(3030, () => console.log(`Server listening on port http://127.0.0.1:3030/`))
